274f0b48c49dc09a3d791e79eb8df83cd28ad105
Bhavani Devulapalli <devulapalli.b@northeastern.edu>
2025-03-20 04:49:55 -0400

    using health api to check db

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
-
 const express = require('express');
 const httpStatus = require('http-status');
 const { health } = require('../controllers/healthController');
 const { uploadFile, getFileMetadata, deleteFile } = require('../controllers/imageController');
 const multer = require('multer');  // Ensure multer is installed for file handling
 const upload = multer({ dest: 'uploads/' });
-
+ 
 const router = express.Router();
-
+ 
 // Middleware to block HEAD requests explicitly
 router.use('/healthz', (req, res, next) => {
   if (req.method === 'HEAD') {
@@ -16,31 +15,17 @@ router.use('/healthz', (req, res, next) => {
   }
   next();
 });
-
+ 
 // Explicitly handling GET requests for /healthz
-router.get('/healthz', health);
-
-
-
+router.get('/healthz', (req, res) => health(req, res, false));
+ 
+ 
+ 
 // Disallowing all other methods (POST, PUT, DELETE, etc.)
 router.all('/healthz', (req, res) => {
   console.log(`Unsupported method: ${req.method}`);
   res.status(405).send();
 });
-
-// // // Register file routes
-// // router.use('/api', fileRoutes); 
-
-// // Registering file routes under /files
-// // Upload a file
-// router.post('/files', upload.single('file'), uploadFile);
-
-// // // Get file metadata
-// // router.get('/files/:id', getFileMetadata);
-
-// // // Delete a file
-// // router.delete('/files/:id', deleteFile);
+ 
+ 
 module.exports = router;
-
-
-
