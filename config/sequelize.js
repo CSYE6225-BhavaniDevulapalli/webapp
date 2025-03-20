2852dff34f708221e31759e56e62432ade6e8442
Bhavani Devulapalli <devulapalli.b@northeastern.edu>
2025-03-20 00:31:34 -0400

    handled db error

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,8 +1,26 @@
+// const { Sequelize } = require('sequelize');
+// const { sqlUri, database } = require('./variables');
+
+// const sequelize = new Sequelize(`${sqlUri}${database}`, {
+//     logging: false
+// });
+
+// exports.sequelize = sequelize;
+
 const { Sequelize } = require('sequelize');
 const { sqlUri, database } = require('./variables');
 
 const sequelize = new Sequelize(`${sqlUri}${database}`, {
-    logging: false
+  logging: false,
+  pool: {
+    max: 5,             // Maximum number of connections in pool
+    min: 0,             // Minimum number of connections in pool
+    acquire: 10000,     // Max time (in ms) to try acquiring a connection before throwing an error
+    idle: 10000         // Time (in ms) before releasing an idle connection
+  },
+  dialectOptions: {
+    connectTimeout: 5000 // Fail connection attempt after 5 seconds if RDS is down
+  }
 });
 
 exports.sequelize = sequelize;
