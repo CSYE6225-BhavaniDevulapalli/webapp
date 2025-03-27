34c181544585b04a73b7c71531d76579fe4bc6db
Bhavani Devulapalli <devulapalli.b@northeastern.edu>
2025-03-27 05:55:50 -0400

    final code for metrics and logging

--- middlewares/s3.js
+++ middlewares/s3.js
@@ -6,39 +6,7 @@ const uuid = require("uuid").v4;
 
 const s3 = new S3();
 
-// exports.s3Uploadv2 = async (file) => {
-//   const id = uuid(); // Generate unique ID
-//   const s3ObjectName = `uploads/${id}-${file.originalname}`;
-  
-//   const params = {
-//     Bucket: process.env.S3_BUCKET_NAME,
-//     Key: s3ObjectName,
-//     Body: file.buffer,
-//     ContentType: file.mimetype,
-//     Metadata: {
-//         'x-amz-meta-original-filename': file.originalname
-//       }
-//     // ACL: 'public-read'
-//   };
 
-//   console.log('S3 Upload Params:', params);
-
-//   try {
-//     const result = await s3.upload(params).promise();
-//     console.log('S3 Upload Success:', result.Location);
-
-//     return {
-//       id,
-//       file_name: file.originalname,
-//       //url: `${process.env.S3_BUCKET_NAME}/${s3ObjectName}`,
-//       url:result.Location,
-//       upload_date: new Date().toISOString()
-//     };
-//   } catch (error) {
-//     console.error('S3 Upload Error:', error.message);
-//     throw new Error(`S3 upload failed: ${error.message}`);
-//   }
-// };
 exports.s3Uploadv2 = async (file) => {
     const id = uuid(); // Generate unique ID
     const s3ObjectName = `uploads/${id}-${file.originalname}`;
@@ -96,16 +64,7 @@ exports.s3GetFile = async (id) => {
     Key: objects.Contents[0].Key
   };
 
-//   try {
-//     const data = await s3.getObject(params).promise();
-//     const metadata = data.Metadata;
-//     return {
-//       //file_name: objects.Contents[0].Key.split('-').slice(1).join('-'),
-//       file_name: metadata['x-amz-meta-original-filename'],
-//       id,
-//       url: 'https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${objects.Contents[0].Key}',
-//       upload_date: objects.Contents[0].LastModified.toISOString()
-//     };
+
 try {
     const data = await s3.getObject(params).promise();
     const metadata = data.Metadata;
