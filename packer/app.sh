2fd4f4d3aa53fe0f9e7412832e3858fe5aa88cb3
Bhavani Devulapalli <devulapalli.b@northeastern.edu>
2025-04-10 19:16:36 -0400

    packer updated with awscli installation

--- packer/app.sh
+++ packer/app.sh
@@ -3,6 +3,14 @@
 # Update and upgrade system packages
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y unzip curl
+
+# Install AWS CLI
+echo "Installing AWS CLI..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf aws awscliv2.zip
+aws --version || { echo "Error: AWS CLI installation failed"; exit 1; }
  
  
 # Install Node.js & npm
